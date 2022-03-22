2d015b109e6cfa8cdab15b6c68dc9372a8b860ef
Mr. Brown <gitcherrypick.work@gmail.com>
2022-03-22 22:47:55 +0500

    Object references.

---
+++ lib/Ch_8/object_references.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# class CelestialBody
+class CelestialBody
+  attr_accessor :name, :type
+
+  bodies = Hash.new do |hash, key|
+    body = CelestialBody.new
+    body.type = 'planet'
+    hash[key] = body
+  end
+
+  bodies['Mars'].name = 'Mars'
+  bodies['Europa'].name = 'Europa'
+  bodies['Europa'].type = 'moon'
+  bodies['Venus'].name = 'Venus'
+
+  bodies.each { |item| p item }
+end
---
+++ spec/Ch_8/object_references_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe '' do
+  # TODO: implement this spec
+end
