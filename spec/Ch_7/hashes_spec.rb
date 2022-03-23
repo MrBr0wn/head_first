0b3d33e8e2d4a4dd21e044e1d54395bbffb2d23e
Mr. Brown <gitcherrypick.work@gmail.com>
2022-03-23 21:55:47 +0500

    Spec for Hashes.

--- spec/Ch_7/hashes_spec.rb
+++ spec/Ch_7/hashes_spec.rb
@@ -2,6 +2,25 @@
 
 require 'Ch_7/hashes'
 
-RSpec.describe '' do
-  # TODO: implement this spec
+RSpec.describe Candidate do
+  subject { described_class.new('Carl Doe', age: 30, occupation: 'QA') }
+
+  it 'should return object of Candidate class' do
+    expect(subject).to be_kind_of(Candidate)
+  end
+
+  it 'should return all instance variables' do
+    instance_variables = %i[@name @age @occupation @hobby @birthplace]
+    expect(subject.instance_variables).to eq(instance_variables)
+  end
+
+  context 'creating object without enough arguments' do
+    it 'should return ArgumentError message with only name' do
+      expect { described_class.new('Test name') }.to raise_error(ArgumentError)
+    end
+
+    it 'should return ArgumentError message with only name and age' do
+      expect { described_class.new('Test name', age: 10) }.to raise_error(ArgumentError)
+    end
+  end
 end
