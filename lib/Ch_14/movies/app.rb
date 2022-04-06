884b171ac23c741019239e40b558a71a6531435c
Mr. Brown <gitcherrypick.work@gmail.com>
2022-04-06 17:02:46 +0500

    Web-app on Sinatra.

---
+++ lib/Ch_14/movies/app.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require './lib/movie'
+
+get('/movies') do
+  @movies = []
+  @movies[0] = Movie.new
+  @movies[0].title = 'Jaws'
+  @movies[1] = Movie.new
+  @movies[1].title = 'Alien'
+  @movies[2] = Movie.new
+  @movies[2].title = 'Terminator 2'
+  erb :index
+end
+
+get('/movies/new') do
+  erb :new
+end
---
+++ lib/Ch_14/movies/lib/movie.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# class Movie
+class Movie
+  attr_accessor :title, :director, :year
+end
